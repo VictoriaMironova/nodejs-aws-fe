18a40528125e683b600f899f91cd916041ef7164
VictoriaMironova <vicky.mironova@gmail.com>
2020-11-01 18:38:24 +0200

    coonet with API

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -2,13 +2,15 @@ import * as Yup from 'yup';
 
 export type Product = {
   id: string,
-  title: string,
-  description: string,
-  price: number,
+  productName: string,
+  productPicture: string,
+  productDescription: string,
+  productCategory: number,
+  productPrice: number  
 };
 
 export const ProductSchema = Yup.object().shape({
-  title: Yup.string().required(),
-  description: Yup.string(),
+  productName: Yup.string().required(),
+  productDescription: Yup.string(),
   price: Yup.number().required(),
 });
